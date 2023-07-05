e6bf3c5fe88a479d55e55166870a48267fb5a896
skripchenkovl <135129597+skripchenkovl@users.noreply.github.com>
2023-07-05 12:00:51 +0300

    Update test-1.sh

--- tests/test-1.sh
+++ tests/test-1.sh
@@ -1,4 +1,8 @@
-from datetime import date
-today = date.today()
-formatted_date = today.strftime('%Y-%m-%d')
-print(f'Today is {formatted_date}')
+#!/bin/bash
+g++ app.cpp -o app
+output=$(./app)
+expected_output="05.07.2023"
+if [ "$output" == "$expected_output" ]; then
+echo "Succesfull: $output"
+else
+echo "Failed. Expected: $expected_output"
